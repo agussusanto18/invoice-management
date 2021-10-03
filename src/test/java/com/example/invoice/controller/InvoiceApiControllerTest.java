3747a70e706949231406a20c3cf7c3893eee3e61
agussusanto18 <otnasussuga.77@gmail.com>
2021-10-03 08:01:18 +0700

    Add automated test for invoiceApiController

--- src/test/java/com/example/invoice/controller/InvoiceApiControllerTest.java
+++ src/test/java/com/example/invoice/controller/InvoiceApiControllerTest.java
@@ -1,9 +1,16 @@
 package com.example.invoice.controller;
 
+import com.example.invoice.dto.CreateInvoiceRequestDto;
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.jdbc.Sql;
+
+import java.math.BigDecimal;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(scripts = {
@@ -11,8 +18,26 @@ import org.springframework.test.context.jdbc.Sql;
         "classpath:/sql/insert-sample-data-invoice.sql"
 })
 public class InvoiceApiControllerTest {
-
     @LocalServerPort
     private Integer serverPort;
 
+    @BeforeEach
+    public void setup() {
+        baseURI = "http://localhost";
+        port = serverPort;
+        System.out.println("Post : " + serverPort);
+    }
+
+    @Test
+    void testCreateInvoice() {
+        CreateInvoiceRequestDto invoiceRequestDto = new CreateInvoiceRequestDto();
+        invoiceRequestDto.setCustomerCode("CUST-001");
+        invoiceRequestDto.setInvoiceTypeCode("REG-001");
+        invoiceRequestDto.setAmount(new BigDecimal(123000.98));
+        invoiceRequestDto.setDescription("Pembayaran Barang 1");
+
+        with().body(invoiceRequestDto).contentType(ContentType.JSON)
+                .when().post("/api/invoice/")
+                .then().statusCode(200);
+    }
 }
